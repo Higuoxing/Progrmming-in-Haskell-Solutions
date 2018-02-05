6a49857f9ed2962071893588c50333b2c575c3d8
higuoxing@gmail.com <higuoxing@outlook.com>
2018-02-05 11:36:27 +0800

    Chapter 4

--- Chapter-4/exp_8.hs
+++ Chapter-4/exp_8.hs
@@ -7,5 +7,5 @@ luhnDouble n | n > 5 = 2 * n - 9
              | otherwise = 2 * n
 
 luhn :: Int -> Int -> Int -> Int -> Bool
-luhn a b c d | (luhnDouble b + luhnDouble c + luhnDouble d) `mod` 10 == 0 = True
+luhn a b c d | (luhnDouble b + luhnDouble d) `mod` 10 == 0 = True
              | otherwise = False
